55b7aa0ac00644cb805ce8c2296988c866623303
Katrín Viktoría Hjartardóttir <kvh2912@gmail.com>
2020-03-24 02:23:05 +0000

    Add supports a single number

--- iha/kata.py
+++ iha/kata.py
@@ -1,5 +1,7 @@
 def Add(Numbers):
     if Numbers == "":
         return 0
+    elif Numbers.isdigit:
+        return int(Numbers)
 
 
--- iha/test_.py
+++ iha/test_.py
@@ -3,3 +3,8 @@ from kata import Add
 
 def test_empty_sting():
     assert Add("") == 0
+
+def test_one_integer():
+    assert Add("1") == 1
+    assert Add("45") == 45
+    assert Add("101") == 101
