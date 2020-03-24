3dc6885a9cf4d24b21614aa8533754dc688b0b1e
Katrín Viktoría Hjartardóttir <kvh2912@gmail.com>
2020-03-24 19:52:09 +0000

    Add supports two numbers separated by comma

--- iha/kata.py
+++ iha/kata.py
@@ -1,7 +1,10 @@
 def Add(Numbers):
     if Numbers == "":
         return 0
+    elif "," in Numbers:
+        num1, num2 = Numbers.split(",")
+        the_sum = int(num1) + int(num2)
+        return the_sum
     elif Numbers.isdigit:
         return int(Numbers)
-
-
+    
--- iha/test_.py
+++ iha/test_.py
@@ -8,3 +8,8 @@ def test_one_integer():
     assert Add("1") == 1
     assert Add("45") == 45
     assert Add("101") == 101
+
+def test_two_integers():
+    assert Add("1,2") == 3
+    assert Add("10,5") == 15
+    assert Add("99,9") == 108
